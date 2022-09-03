5feb02f800fb5e4b6b2448415c8878e3be6a5901
Michael Jung <MichaelWJung@users.noreply.github.com>
2022-09-03 22:13:22 +0200

    Write funcs creating data for the account screen

--- src/main/money/screens/account_screen.cljs
+++ src/main/money/screens/account_screen.cljs
@@ -15,8 +15,30 @@
                (ledger-entries-contain-account? account-id))
           transactions))
 
-(defn- get-date [[k v]]
-  (::t/date v))
+(defn remove-nth [coll n]
+  [(get coll n) (concat (take n coll) (drop (inc n) coll))])
+
+(defn go-over-ledger-entries [entries]
+  (for [n (range 0 (count entries))]
+    (remove-nth entries n)))
+
+(defn get-ledger-entries-for-account [entries account-id]
+  (filter #(= account-id (::le/account-id (first %)))
+          (go-over-ledger-entries entries)))
+
+(defn create-modified-account-screen-transaction [transaction account-id]
+  (let [ledger-entries (::t/ledger-entries transaction)
+        without-ledger-entries (dissoc transaction ::t/ledger-entries)]
+    (map #(-> without-ledger-entries
+              (assoc :account-ledger-entry (first %))
+              (assoc :other-ledger-entries (second %)))
+         (get-ledger-entries-for-account ledger-entries account-id))))
 
 (defn sort-transactions-by-date [transactions]
-  (sort-by get-date transactions))
+  (sort-by ::t/date transactions))
+
+(defn build-account-screen-transactions [transactions account-id]
+  (sort-transactions-by-date
+    (flatten (map #(create-modified-account-screen-transaction
+                     (second %) account-id)
+                  transactions))))
--- src/test/money/screens/account_screen_test.cljs
+++ src/test/money/screens/account_screen_test.cljs
@@ -5,34 +5,28 @@
             [money.core.transaction :as t]
             [money.screens.account-screen :as as]))
 
+(defn- ledger-entry [account-id amount]
+  {::le/account-id account-id
+   ::le/amount {::m/amount amount
+                ::m/currency-id 0}})
+
 (def ^:private transactions
   (sorted-map
     2 {::t/description "Cinema"
        ::t/date 3000000
-       ::t/ledger-entries [{::le/account-id 3
-                            ::le/amount {::m/amount 2230
-                                         ::m/currency-id 0}}
-                           {::le/account-id 6
-                            ::le/amount {::m/amount -2230
-                                         ::m/currency-id 0}}]}
+       ::t/ledger-entries [(ledger-entry 3 2230)
+                           (ledger-entry 6 -2230)]}
 
     5 {::t/description "Bowling"
        ::t/date 1000000
-       ::t/ledger-entries [{::le/account-id 11
-                            ::le/amount {::m/amount 3300
-                                         ::m/currency-id 0}}
-                           {::le/account-id 6
-                            ::le/amount {::m/amount -3300
-                                         ::m/currency-id 0}}]}
+       ::t/ledger-entries [(ledger-entry 11 3300)
+                           (ledger-entry 6 -3300)]}
 
     6 {::t/description "Playing pinball"
        ::t/date 2000000
-       ::t/ledger-entries [{::le/account-id 3
-                            ::le/amount {::m/amount 1200
-                                         ::m/currency-id 0}}
-                           {::le/account-id 6
-                            ::le/amount {::m/amount -1200
-                                         ::m/currency-id 0}}]}))
+       ::t/ledger-entries [(ledger-entry 3 1000)
+                           (ledger-entry 3 200)
+                           (ledger-entry 6 -1200)]}))
 
 (deftest filter-transactions-by-account-test
   (testing "Only returns transactions whose ledger entries contain account id"
@@ -43,9 +37,73 @@
     (is (= (as/filter-transactions-by-account transactions 3)
            (seq (select-keys transactions [2 6]))))))
 
+(deftest remove-nth-test
+  (is (= (as/remove-nth [1 2 3 4 5] 2) [3 [1 2 4 5]]))
+  (is (= (as/remove-nth [1 2 3 4 5] 0) [1 [2 3 4 5]]))
+  (is (= (as/remove-nth [1 2 3 4 5] 4) [5 [1 2 3 4]]))
+  (is (= (as/remove-nth [1 2 3 4 5] 5) [nil [1 2 3 4 5]])))
+
+(deftest go-over-ledger-entries-test
+  (let [e1 (ledger-entry 0 100)
+        e2 (ledger-entry 1 200)
+        e3 (ledger-entry 2 -300)]
+    (is (= (as/go-over-ledger-entries [e1 e2 e3])
+           [[e1 [e2 e3]] [e2 [e1 e3]] [e3 [e1 e2]]]))))
+
+(deftest get-ledger-entries-for-account-test
+  (let [e1 (ledger-entry 0 100)
+        e2 (ledger-entry 1 200)
+        e3 (ledger-entry 0 -300)]
+    (is (= (as/get-ledger-entries-for-account [e1 e2 e3] 0)
+           [[e1 [e2 e3]] [e3 [e1 e2]]]))))
+
+(deftest create-modified-account-screen-transaction-test
+  (testing
+    (str "Replaces ::t/ledger-entries by :account-ledger-entry and "
+         ":other-ledger-entries")
+    (is (= (as/create-modified-account-screen-transaction
+             (get transactions 2) 3)
+           [{::t/description "Cinema"
+             ::t/date 3000000
+             :account-ledger-entry (ledger-entry 3 2230)
+             :other-ledger-entries [(ledger-entry 6 -2230)]}])))
+  (testing
+    (str "Returns multiple transactions if an account is used multiple times "
+         "in ledger entries")
+    (is (= (as/create-modified-account-screen-transaction
+             (get transactions 6) 3)
+           [{::t/description "Playing pinball"
+             ::t/date 2000000
+             :account-ledger-entry (ledger-entry 3 1000)
+             :other-ledger-entries [(ledger-entry 3 200)
+                                    (ledger-entry 6 -1200)]}
+            {::t/description "Playing pinball"
+             ::t/date 2000000
+             :account-ledger-entry (ledger-entry 3 200)
+             :other-ledger-entries [(ledger-entry 3 1000)
+                                    (ledger-entry 6 -1200)]}]))))
+
 (deftest sort-transactions-by-date-test
-  (testing "Transactions are sorted ascendingly by date"
-    (is (= (as/sort-transactions-by-date transactions)
-           (seq (select-keys transactions [5 6 2]))))
-    (is (= (as/sort-transactions-by-date (seq transactions))
-           (seq (select-keys transactions [5 6 2]))))))
+  (testing "Transactions are sorted by date in ascending order"
+    (is (= (as/sort-transactions-by-date (vals transactions))
+           (map transactions [5 6 2])))))
+
+(deftest build-account-screen-transactions-test
+  (testing
+    (str "Collects all modified transactions from originating from incoming "
+         "transactions")
+    (is (= (as/build-account-screen-transactions transactions 3)
+           [{::t/description "Playing pinball"
+             ::t/date 2000000
+             :account-ledger-entry (ledger-entry 3 1000)
+             :other-ledger-entries [(ledger-entry 3 200)
+                                    (ledger-entry 6 -1200)]}
+            {::t/description "Playing pinball"
+             ::t/date 2000000
+             :account-ledger-entry (ledger-entry 3 200)
+             :other-ledger-entries [(ledger-entry 3 1000)
+                                    (ledger-entry 6 -1200)]}
+            {::t/description "Cinema"
+             ::t/date 3000000
+             :account-ledger-entry (ledger-entry 3 2230)
+             :other-ledger-entries [(ledger-entry 6 -2230)]}]))))
